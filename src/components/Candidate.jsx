25b4afcf71a153698f43ed3655388efc45f4d6d6
Mourad <mourad.eyes@gmail.com>
2019-12-17 22:53:56 +0100

    add people page

---
+++ src/components/Candidate.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import avatar from "../avatar.jpg";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: left;
+  width: 50rem;
+  margin-bottom: 4rem;
+`;
+
+const Image = styled.div`
+  border-radius: 0.4rem;
+  overflow: hidden;
+  margin-right: 3rem;
+
+  img {
+    height: 25rem;
+    width: 25rem;
+  }
+`;
+
+const ParamList = styled.ul`
+  margin-top: 4rem;
+
+  li {
+    margin-bottom: 3rem;
+    list-style: none;
+    font-size: 1.6rem;
+    font-weight: 500;
+  }
+`;
+
+const Candidate = () => {
+  return (
+    <Container>
+      <Image>
+        <img src={avatar} />
+      </Image>
+      <ParamList>
+        <li>Copy link</li>
+        <li>edit</li>
+        <li>duplicate</li>
+        <li>delete</li>
+      </ParamList>
+    </Container>
+  );
+};
+
+export default Candidate;
--- src/components/pages/People.jsx
+++ src/components/pages/People.jsx
@@ -1,8 +1,76 @@
 import React from "react";
 import styled from "styled-components";
+import urPeople from "../../ur-people.svg";
+import Condidate from "../Candidate";
+
+const Container = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  margin-top: 3rem;
+`;
+
+const Nav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Logo = styled.div`
+  margin-top: 1.2rem;
+`;
+
+const Search = styled.div`
+  input {
+    all: unset;
+    width: 30rem;
+    background: #fff;
+    border: 1px solid #ddd;
+    border-radius: 0.3rem;
+    padding: 1rem;
+    font-size: 1.3rem;
+  }
+`;
+
+const Condidates = styled.div`
+  display: flex;
+  justify-content: left;
+  flex-wrap: wrap;
+`;
+
+const Banner = styled.div`
+  background-color: #fff;
+  border-radius: 0.8rem;
+  padding: 2rem 1rem;
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.1);
+  margin-top: 5rem;
+  margin-bottom: 4rem;
+
+  h1 {
+    font-weight: 700;
+  }
+`;
 
 const People = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Nav>
+        <Logo>
+          <img src={urPeople} alt="UR People" />
+        </Logo>
+        <Search>
+          <input type="text" placeholder="Search candidate" />
+        </Search>
+      </Nav>
+      <Banner>
+        <h1>All Condidates</h1>
+      </Banner>
+      <Condidates>
+        <Condidate></Condidate>
+        <Condidate></Condidate>
+        <Condidate></Condidate>
+      </Condidates>
+    </Container>
+  );
 };
 
 export default People;
