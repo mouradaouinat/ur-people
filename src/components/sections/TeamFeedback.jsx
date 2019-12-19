822716afaa45c4da3e6c31b26a7d9c03bf428eff
Mourad <mourad.eyes@gmail.com>
2019-12-19 18:20:29 +0100

    Fix minor layout issues

--- src/components/sections/TeamFeedback.jsx
+++ src/components/sections/TeamFeedback.jsx
@@ -6,9 +6,13 @@ const Container = styled.div`
   border-radius: 1rem;
   padding: 1rem;
   margin-top: 3rem;
-  position: relative;
   margin-bottom: 3rem;
   box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  margin-bottom: 3rem;
 `;
 
 const ContentHeading = styled.div`
@@ -37,35 +41,64 @@ const Rating = styled.span`
   color: #6050dc;
   position: absolute;
   right: 1rem;
-  top: 2rem;
+  top: 1rem;
   margin-right: 0.5rem;
 `;
 
 const TeamFeedback = () => (
   <Container>
-    <ContentHeading>
-      <div>
-        <img
-          src="https://ca.slack-edge.com/T02QFPDEX-U02QFPDEZ-81d30bcde900-512"
-          alt="Owner of UR"
-        />
-      </div>
-      <div>
-        <h3>Yassine Kachchani</h3>
-        <span>Talent Aquisition</span>
-      </div>
-      <Rating>
-        Yes{" "}
-        <span role="img" aria-label="emoji">
-          👍
-        </span>
-      </Rating>
-    </ContentHeading>
-    <FeedBack>
-      Has proven ability to produce creative, original ideas, plans, products or
-      methods even if they’re not always practical, in line with organization
-      needs, or successful. - Passion - Culture fit - Communication - Collab
-    </FeedBack>
+    <Wrapper>
+      <ContentHeading>
+        <div>
+          <img
+            src="https://ca.slack-edge.com/T02QFPDEX-U02QFPDEZ-81d30bcde900-512"
+            alt="Owner of UR"
+          />
+        </div>
+        <div>
+          <h3>Yassine Kachchani</h3>
+          <span>Talent Aquisition</span>
+        </div>
+        <Rating>
+          Yes{" "}
+          <span role="img" aria-label="emoji">
+            👍
+          </span>
+        </Rating>
+      </ContentHeading>
+      <FeedBack>
+        Has proven ability to produce creative, original ideas, plans, products
+        or methods even if they’re not always practical, in line with
+        organization needs, or successful. - Passion - Culture fit -
+        Communication - Collab
+      </FeedBack>
+    </Wrapper>
+    <Wrapper>
+      <ContentHeading>
+        <div>
+          <img
+            src="https://ca.slack-edge.com/T02QFPDEX-U0F3ZM26M-8bb4a417dc1d-512"
+            alt="Owner of UR"
+          />
+        </div>
+        <div>
+          <h3>Zack Braska</h3>
+          <span>Talent Aquisition</span>
+        </div>
+        <Rating>
+          Strong Yes{" "}
+          <span role="img" aria-label="emoji">
+            👍👍
+          </span>
+        </Rating>
+      </ContentHeading>
+      <FeedBack>
+        Has proven ability to produce creative, original ideas, plans, products
+        or methods even if they’re not always practical, in line with
+        organization needs, or successful. - Passion - Culture fit -
+        Communication - Collab
+      </FeedBack>
+    </Wrapper>
   </Container>
 );
 
